1b4f231f6e879b26ceb8f5987903ed673bc54177
hummingjay <jays7mill@gmail.com>
2023-07-24 00:20:45 +0300

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,13 +5,13 @@ script that starts a Flask web application:
 """
 
 from flask import FLASK
-app = Flask(__name)
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """Return Hello world!"""
-    return ('Hello HBNB')
+    """Return Hello HBNB!!"""
+    return ('Hello HBNB!')
 
 
 @app.route('/hbnb', strict_slashes=False)
