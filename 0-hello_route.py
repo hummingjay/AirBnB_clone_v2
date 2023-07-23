1f96180f664f2591351d7cf9a624a9d5b5de1289
hummingjay <jays7mill@gmail.com>
2023-07-23 23:58:02 +0300

    0-hello_route.py

---
+++ 0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Flask Web application displays "Hello HBNB!"
+"""
+
+from flask import Flask
+
+# create flask instance
+app = Flask(__name__)
+
+# create a router with a decorator
+# / first page
+
+
+@app.route('/', strict_slashes=false)
+def index():
+    return ("Hello HBNB!")
+
+
+if __name__ == '__main__':
+    # Specify ip to run on and port to listen to
+    app.run(host='0.0.0.0', port=5000)
