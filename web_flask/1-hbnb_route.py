40010949a4544f67298b5ff88e8f020dd186d5be
hummingjay <jays7mill@gmail.com>
2023-07-24 00:28:45 +0300

    argh

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """Return Hello HBNB!"""
-    return ('Hello HBNB!')
+    return ("Hello HBNB!")
 
 
 @app.route('/hbnb', strict_slashes=False)
